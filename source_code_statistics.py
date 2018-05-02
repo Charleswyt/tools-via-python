1fb774892235bf74b599988b9b3235ad3baf1b71
Charles_wyt <wangyuntao16@mails.ucas.ac.cn>
2018-05-02 11:18:11 +0800

    source code statistics
    实现任意语言的代码量统计

---
+++ source_code_statistics.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Created on 2018.5.2
+Finished on 2018.5.2
+@author: Wang Yuntao
+"""
+
+import os
+import time
+import win_unicode_console
+
+win_unicode_console.enable()
+
+def get_file_path(file_folder, file_types):
+    """
+    获取待统计文件的路径
+    :param file_folder: 代码所在文件路径
+    :param file_types: 待统计文件的类型
+    :retuen:
+        file_list: 当前文件夹下的文件列表(完整路径)
+    """
+    file_path_list = list()
+    for parent, dirnames, filenames in os.walk(file_folder):
+        for filename in filenames:
+            extension = filename.split('.')[-1]
+            if extension in file_types:
+                file_path_list.append(parent + "/" + filename)
+    
+    return file_path_list
+
+
+def get_file_name(file_path):
+    """
+    根据文件路径获取文件名
+    :param file_path: 文件路径
+    :return:
+        file_name: 文件名
+    """
+    file_name = file_path.split("/")[-1]
+
+    return file_name
+
+
+def count_line(file_name):
+    """
+    统计文件的内容行数
+    :param file_name: 文件名
+    :return:
+        count: 当前文件的内容行数
+    """
+    count = 0
+    for file_line in open(file_name, encoding="utf-8", errors='ignore').readlines():
+        if file_line != '' and file_line != '\n':  # 过滤掉空行
+            count += 1
+
+    return count
+
+
+def get_lines(file_folder, file_types):
+    """
+    获取当前文件夹下的代码量
+    :param file_folder: 代码所在文件路径
+    :param file_types: 待统计文件的类型
+    """
+    file_path_list = get_file_path(file_folder, file_types)
+    index, total_lines = 0, 0
+    
+    template_title = "{:^5}\t{:36}{:^15}"
+    template_content = "{:^5}\t{:36}{:^15}"
+    print(template_title.format("ID", "file name", "lines"))
+    print("=======================================================")
+    
+    for file_path in file_path_list:
+        file_name = get_file_name(file_path)
+        lines = count_line(file_path)
+        total_lines = total_lines + lines
+        index += 1
+        print(template_content.format(index, file_name, lines))
+
+    print("=======================================================")
+    print("total lines: %d" % total_lines)
+    
+
+if __name__ == '__main__':
+    startTime = time.clock()
+    file_folder = "E:/Myself/1.source_code/audio_steganalysis"
+    file_types = ["cpp"]
+    print("file floder: %s" % file_folder)
+    get_lines(file_folder, file_types)
+    print('Done! Cost Time: %0.2f second' % (time.clock() - startTime))
